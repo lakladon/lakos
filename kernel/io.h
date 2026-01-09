b7c750e59ceac27eb47a03ac012679829716bfa0
lakladon <blb290965@gmail.com>
2026-01-09 21:35:22 +0700

    Update io.h

--- kernel/io.h
+++ kernel/io.h
@@ -3,21 +3,14 @@
 
 #include <stdint.h>
 
-// Отправка байта в порт
 static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
-// Чтение байта из порта
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
     __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
-}
-
-// Небольшая задержка (нужна для старого железа и PIC)
-static inline void io_wait(void) {
-    outb(0x80, 0);
 }
 
 #endif
