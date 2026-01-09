0777d95be76010db4716501ea510b0a866b0697e
lakladon <blb290965@gmail.com>
2026-01-09 15:56:00 +0700

    Update idt.h

--- kernel/include/idt.h
+++ kernel/include/idt.h
@@ -1,23 +1,25 @@
+#ifndef IDT_H
+#define IDT_H
+
 #include <stdint.h>
 
-
+// Структура записи IDT
 struct idt_entry_struct {
     uint16_t base_low;
     uint16_t sel;
-    uint8_t always0;
-    uint8_t flags;
+    uint8_t  always0;
+    uint8_t  flags;
     uint16_t base_high;
-} _attribute__((packed));
-
-struct idt_ptr_struct
-{
-    /* data */
-    uint16_t limit;
-    uint32_t base;
-}__attribute__((packed));
+} __attribute__((packed)); // ИСПРАВЛЕНО: должно быть два подчеркивания в начале
 
 typedef struct idt_entry_struct idt_entry_t;
+
+// Структура указателя IDT (для команды lidt)
+struct idt_ptr_struct {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed)); // ИСПРАВЛЕНО: должно быть два подчеркивания в начале
+
 typedef struct idt_ptr_struct idt_ptr_t;
 
-void init_idt();
-
+#endif
