4d949f1d350645d722764731152220b61c76cf71
lakladon <blb290965@gmail.com>
2026-01-10 01:05:13 +0700

    Fix typos in interrupt handler references

--- kernel/interrupts.asm
+++ kernel/interrupts.asm
@@ -1,43 +1,24 @@
+; Исправленный фрагмент kernel/interrupts.asm
 [BITS 32]
-[GLOBAL idt_load]
-[EXTERN isr_heandler]
-%macro ISR_NOERR 1
-[GLOBAL isr%1]
-isr%1:
-    push byte 0
-    push byte %1 
-    jmp isr_common_stub
-%endmacro
-
-%macro ISR_ERR 1
-[GLOBAL isr%1]
-isr%1:
-    push byte %1 
-    jmp isr_common_stub
-%endmacro
-
-ISR_NOERR 0
-ISR_NOERR 1
-ISR_NOERR 8
-ISR_NOERR 13
-ISR_NOERR 14
+extern isr_handler    ; Исправлено с heandler на handler
 
 isr_common_stub:
     pusha
+    mov ax, ds
+    push eax
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-
-    call isr_heandler
-
+    
+    call isr_handler  ; Исправлено с heandler на handler
+    
     pop eax
     mov ds, ax
-    mov es, ax  
+    mov es, ax
     mov fs, ax
     mov gs, ax
-
-    popa 
+    popa
     add esp, 8
     iret
-    
