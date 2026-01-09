473990db3c805d067de405077d387fd78d9d172d
lakladon <blb290965@gmail.com>
2026-01-10 01:02:43 +0700

    Update gdtflush.asm

--- kernel/gdtflush.asm
+++ kernel/gdtflush.asm
@@ -1,15 +1,20 @@
-global gdt_flush
+[BITS 32]
+global gdt_flush     ; Объявляем функцию глобальной для линковщика
 
 gdt_flush:
-    mov eax, [esp+4] 
-    lgdt [eax]
+    mov eax, [esp + 4] ; Берем указатель на gdt_ptr из стека
+    lgdt [eax]         ; Загружаем GDT
 
-    mov ax, 0x10
+    ; Перезагружаем сегменты данных
+    mov ax, 0x10       ; 0x10 — смещение сегмента данных в GDT
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    jmp 0x08:.gdt_flush
+
+    ; Дальний прыжок для перезагрузки сегмента кода (CS)
+    jmp 0x08:.flush    ; 0x08 — смещение сегмента кода
+
 .flush:
     ret
