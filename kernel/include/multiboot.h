1a337d5b034f09d12ec4471ccd49777883f8babc
lakladon <blb290965@gmail.com>
2026-01-09 15:53:27 +0700

    Add multiboot header definitions and structures

---
+++ kernel/include/multiboot.h
@@ -0,0 +1,43 @@
+#ifndef MULTIBOOT_H
+#define MULTIBOOT_H
+
+#include <stdint.h>
+
+#define MULTIBOOT_SEARCH                        8192
+#define MULTIBOOT_HEADER_MAGIC                  0x1BADB002
+#define MULTIBOOT_BOOTLOADER_MAGIC              0x2BADB002
+
+struct multiboot_module {
+    uint32_t mod_start;
+    uint32_t mod_end;
+    uint32_t cmdline;
+    uint32_t pad;
+};
+typedef struct multiboot_module multiboot_module_t;
+
+struct multiboot_info {
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t boot_device;
+    uint32_t cmdline;
+    uint32_t mods_count;
+    uint32_t mods_addr;
+    uint32_t syms[4];
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+    uint32_t drives_length;
+    uint32_t drives_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+};
+typedef struct multiboot_info multiboot_info_t;
+
+#endif
