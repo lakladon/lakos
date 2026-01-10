9a39b2717da913f49c54e0cb1f57ed46de9e006a
lakladon <blb290965@gmail.com>
2026-01-11 06:28:27 +0700

    123

--- kernel/shell.c
+++ kernel/shell.c
@@ -735,9 +735,7 @@ void shell_main() {
     terminal_writestring("Login successful\n");
     terminal_writestring("LakOS>");
     terminal_writestring(current_user);
-    terminal_writestring(" ");
-    terminal_writestring(current_dir);
-    terminal_writestring(" ");
+    terminal_writestring(" / ");
     while(1) {
         if (inb(0x64) & 0x1) {
             uint8_t scancode = inb(0x60);
