384715d9eff356381212fd6beab8eec64d6a3fb9
maksim <blb290965@gmail.com>
2026-02-03 02:06:51 +0700

    1

--- kernel/commands.c
+++ kernel/commands.c
@@ -5,6 +5,8 @@
 #include "include/version.h"
 #include "include/commands.h"
 #include <io.h>
+#include <string.h>
+#include <stddef.h>
 
 extern void terminal_writestring(const char* s);
 extern void terminal_putchar(char c);
